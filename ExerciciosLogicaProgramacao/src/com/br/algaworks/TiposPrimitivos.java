4a11224f59f2bd475c4b14c19d9a5300b6621a3e
sioliv <silvinha.olivert@gmail.com>
2022-07-14 22:06:19 -0300

    Tipos primitivos. Não encontram-se em estados normais de ausência de valor.

---
+++ ExerciciosLogicaProgramacao/src/com/br/algaworks/TiposPrimitivos.java
@@ -0,0 +1,24 @@
+package com.br.algaworks;
+
+public class TiposPrimitivos {
+	public static void main(String[] args) {
+		//Variáveis primitivas aceitam valores null
+		
+		String variavelString = "string";
+		
+		Byte variavelByte = 1;
+		
+		Short variavelShort = 1;
+		
+		Integer variavelInteger = 1;
+		
+		Long variavelLong = 1L;
+		
+		Float variavelFloat = 1.0f;
+		
+		Character variavelCharacter = 'A';
+		
+		Boolean variavelBoolean = true;
+	}
+
+}
