94b5ddf205ba7465a17068fb4037a2b24919b82c
sioliv <silvinha.olivert@gmail.com>
2022-07-19 19:48:48 -0300

    Exemplo de concatenação de strings

---
+++ ExerciciosLogicaProgramacao/src/CursoLogicaCapitulo04/ConcatenacaoTextos.java
@@ -0,0 +1,14 @@
+package CursoLogicaCapitulo04;
+
+public class ConcatenacaoTextos {
+
+	public static void main(String[] args) {
+		String inicio = "Esse "; //  String vazia quando abro e fecho aspas duplas
+		String meio = "É um exemplo ";
+		String fim = "de concatenação de textos.";
+		
+		System.out.println(inicio + meio + fim);
+
+	}
+
+}
