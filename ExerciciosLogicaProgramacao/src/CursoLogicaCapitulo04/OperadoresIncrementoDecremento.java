04db58748ead096b6ea33648528799dca084247e
sioliv <silvinha.olivert@gmail.com>
2022-07-19 19:55:14 -0300

    Exemplo de incremento e decremento

---
+++ ExerciciosLogicaProgramacao/src/CursoLogicaCapitulo04/OperadoresIncrementoDecremento.java
@@ -0,0 +1,16 @@
+package CursoLogicaCapitulo04;
+
+public class OperadoresIncrementoDecremento {
+
+	public static void main(String[] args) {
+		Integer numero = 10;
+		
+		++numero;
+		System.out.println(numero);
+		
+		numero--;
+		System.out.println(numero);
+
+	}
+
+}
