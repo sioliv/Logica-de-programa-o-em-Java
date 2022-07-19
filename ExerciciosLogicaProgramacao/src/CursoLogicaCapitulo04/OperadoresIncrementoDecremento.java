dddd1a90f02d4f884ecf8cd86ea0354acf2271c8
sioliv <silvinha.olivert@gmail.com>
2022-07-19 19:58:50 -0300

    Mas exemplos de operações com incremento e decremento

--- ExerciciosLogicaProgramacao/src/CursoLogicaCapitulo04/OperadoresIncrementoDecremento.java
+++ ExerciciosLogicaProgramacao/src/CursoLogicaCapitulo04/OperadoresIncrementoDecremento.java
@@ -6,10 +6,15 @@ public class OperadoresIncrementoDecremento {
 		Integer numero = 10;
 		
 		++numero;
-		System.out.println(numero);
+		System.out.println("Numero depois:" + numero);
 		
 		numero--;
-		System.out.println(numero);
+		System.out.println("Numero antes do depois:" + numero);
+		
+		//Integer numero02 = ++numero;
+		Integer numero02 = numero++;
+		
+		System.out.println("Numero: " + numero + "," + "numero02 " + numero02);
 
 	}
 
