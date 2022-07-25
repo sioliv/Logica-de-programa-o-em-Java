467f081e02e079d7432f04e74fbb1b3b76b0235a
sioliv <silvinha.olivert@gmail.com>
2022-07-24 22:51:04 -0300

    Calcule o bônus do funcionário(a)

---
+++ ExerciciosLogicaProgramacao/src/CursoLogicaCapitulo05/Exercicio02.java
@@ -0,0 +1,46 @@
+package CursoLogicaCapitulo05;
+
+import java.util.Scanner;
+
+public class Exercicio02 {
+	
+	//
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Digite a meta do faturamento anual da empresa");
+		Double metaDoFaturamentoAnual = scanner.nextDouble();
+		
+		System.out.println("Digite o faturamento real da empresa no ultimo ano");
+		Double faturamentoRealDaEmpresa = scanner.nextDouble();
+		
+		System.out.println("Digite a média salarial do funcionario(a) ");
+		Double mediaSalarialDoFuncionario = scanner.nextDouble();
+		
+		Double oitentaPorCentoDoValorDaMeta = (metaDoFaturamentoAnual * 8.0) / 10;
+		
+		Boolean faturamentoParaBonus = faturamentoRealDaEmpresa >= metaDoFaturamentoAnual;
+		Boolean faturamentoMenorQueAmeta =  (faturamentoRealDaEmpresa < metaDoFaturamentoAnual) && (faturamentoRealDaEmpresa >= oitentaPorCentoDoValorDaMeta);
+		
+		
+		Double bonus = 0.0;
+		Double bonusFaturamentoMenorQueMeta = 0.0;
+		
+		if(faturamentoParaBonus) {
+			bonus = bonus + mediaSalarialDoFuncionario;
+			
+			System.out.println("Você ganhou bonus integral!" + bonus);
+			
+		} else if (faturamentoMenorQueAmeta ) {
+			bonusFaturamentoMenorQueMeta = bonusFaturamentoMenorQueMeta + oitentaPorCentoDoValorDaMeta;
+			
+			System.out.println(bonusFaturamentoMenorQueMeta);
+		} else {
+			System.out.println("Infelizmente, esse ano não tem bônus!");
+		}
+		
+		scanner.close();
+	}
+
+	
+}
