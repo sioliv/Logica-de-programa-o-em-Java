5ca54049a2c9d4ca432687b238c10b202bdf2b0d
sioliv <silvinha.olivert@gmail.com>
2022-07-24 21:01:38 -0300

    Outra maneira de fazer

---
+++ ExerciciosLogicaProgramacao/src/CursoLogicaCapitulo04/Exercicio04OutraManeiraDeFazer.java
@@ -0,0 +1,20 @@
+package CursoLogicaCapitulo04;
+
+import java.util.Scanner;
+
+public class Exercicio04OutraManeiraDeFazer {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Digite a primeira nota do candidato: ");
+		Double primeiraNotaCandidato = entrada.nextDouble();
+		
+		System.out.println("Digite a segunda nota do candidato: ");
+		Double segundaNotaCandidato = entrada.nextDouble();
+		
+		
+
+	}
+
+}
