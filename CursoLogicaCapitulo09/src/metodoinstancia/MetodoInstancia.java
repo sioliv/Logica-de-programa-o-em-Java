38f76f75787a112075063de91d8a743abfae56a9
sioliv <silvinha.olivert@gmail.com>
2022-08-18 23:59:28 -0300

    main

--- CursoLogicaCapitulo09/src/metodoinstancia/MetodoInstancia.java
+++ CursoLogicaCapitulo09/src/metodoinstancia/MetodoInstancia.java
@@ -11,14 +11,20 @@ public class MetodoInstancia {
 		
 		cliente.primeiroNome = "Alexandre";
 		cliente.segundoNome = "Afonso";
-		cliente.telefone = "0000000000";
+		cliente.telefone = "2100000000";
 		cliente.email = "alexandre.afonso@algaworks.com";
+		
+		Cliente cliente2 = new Cliente();
+		cliente2.primeiroNome = "Joao";
+		cliente2.segundoNome = "Alves";
+		cliente2.telefone = "34888888888";
+		cliente2.email = "joao@algaworks.com";
 		
 		//Toda vez eu declaro a variável e uso o método, é um método de instância.
 		//Não cria variáveis estáticas dentro de uma classe que visa bstrair conceitos. Ex dentro da classe Cliente, não posso criar métodos estáticos
 		 //invocando o método da instância   
-		System.out.println("Nome cliente: " + cliente.obterNomeCompletoCliente());
-		
+		System.out.println("Nome cliente: " + cliente.obterNomeCompletoCliente() + ", DDD " + cliente.obterDDD());
+		System.out.println("Nome cliente: " + cliente2.obterNomeCompletoCliente() + ", DDD " + cliente2.obterDDD());
 	}
 	
 //	static String obterNomeCompletoCliente(Cliente cliente) {
