119415ba3bfaa361d3ed6c535001dc8f78cb0798
sioliv <silvinha.olivert@gmail.com>
2022-08-22 00:30:45 -0300

    classe produto

---
+++ CursoLogicaCapitulo09/src/classeInstancia/Produto.java
@@ -0,0 +1,12 @@
+package classeInstancia;
+
+public class Produto {
+	
+	//ctrl + shitf + x para deixar com letra maiuscula
+	//ctrl shitf y - deixa minusculo
+	
+	//Classe é a estrutura que me permite criar uma instância.
+	static Integer quantidade_minima_estoque = 1;
+	String nome;
+
+}
