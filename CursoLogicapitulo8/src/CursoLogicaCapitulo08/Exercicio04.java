4f2d7fcb70e7d817df52af6b9ad7e3d4f1b1e65d
sioliv <sioliv@github.com>
2022-09-25 17:09:22 -0300

    implementacao

--- CursoLogicapitulo8/src/CursoLogicaCapitulo08/Exercicio04.java
+++ CursoLogicapitulo8/src/CursoLogicaCapitulo08/Exercicio04.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Exercicio04 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Digite o numero :");
+		System.out.println("Digite o numero ::");
 		Integer numero = scanner.nextInt();
 		
 		imprimirTabuada(numero, 0);
